630e46c2386faaef7ca694221893b6525434cbd2
MartinMwiti <martinmwiti007@gmail.com>
2020-01-21 21:51:02 +0300

    update

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
